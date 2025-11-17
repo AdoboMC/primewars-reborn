fd3b897cdbb8a58c8524df281a7304c93e1f359d
markjordanugtongspc <markjordan.ugtong.202200752@gmail.com>
2025-11-17 16:57:04 +0800

    Initial commit with main folder, gitignore, and gitattributes

---
+++ main/start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java -Xms2G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=100 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -jar server.jar nogui
+PAUSE
